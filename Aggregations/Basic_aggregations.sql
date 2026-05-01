a95a4d96c162212f46158380c3b40c8a1361fa73
Sello Letswalo <sello.letswalo07@gmail.com>
2026-05-01 23:33:40 +0200

    Basic_aggregations.sql

--- Aggregations/Basic_aggregations.sql
+++ Aggregations/Basic_aggregations.sql
@@ -1,6 +1,47 @@
+-- Purpose: Count total number of orders
+-- Concept: COUNT aggregation
+-- Dataset: orders table
+
+SELECT COUNT(*) AS total_orders
+FROM orders;
+
+------------------------------------------------------------------
+------------------------------------------------------------------
+
+-- Purpose: Calculate total revenue
+-- Concept: SUM aggregation
+-- Dataset: orders table
+
+SELECT SUM(amount) AS total_revenue
+FROM orders;
+
+------------------------------------------------------------------
+------------------------------------------------------------------
+
+-- Purpose: Calculate average order value
+-- Concept: AVG aggregation
+-- Dataset: orders table
+
+SELECT AVG(amount) AS avg_order_value
+FROM orders;
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+
+-- Purpose: Find the smallest order amount
+-- Concept: MIN aggregation
+-- Dataset: orders table
+
+SELECT MIN(amount) AS smallest_order
+FROM orders;
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+
 -- Purpose: Find the largest order amount
 -- Concept: MAX aggregation
 -- Dataset: orders table
 
 SELECT MAX(amount) AS largest_order
 FROM orders;
+
